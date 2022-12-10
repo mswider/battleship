618d80abe367a6d985896f9d497b34bbf2438a13
mswider <matthew.swider@outlook.com>
2022-12-10 15:21:19 -0600

    Make Game class and add some routes/middleware

--- index.js
+++ index.js
@@ -2,19 +2,35 @@ import express from 'express';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import morgan from 'morgan';
+import crypto from 'crypto';
 
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
+
+function log() {
+    console.log(new Date().toLocaleString() + ' -', ...arguments);
+}
 
 const argv = yargs(hideBin(process.argv))
   .option('port', {
     alias: 'p',
     default: 3000,
     describe: 'Configures the port used for the web server'
+  }).option('code', {
+    alias: 'c',
+    default: 6,
+    describe: 'Sets the length of game codes'
+  }).option('timeout', {
+    alias: 't',
+    default: 90,
+    describe: 'Deletes inactive games after this many seconds'
   }).argv;
 
 const port = argv.port;
+const codeLength = argv.code;
+const timeout = argv.timeout;
+const maxGames = 10 ** codeLength;
 
 const home = `
 <!DOCTYPE html>
@@ -51,11 +67,87 @@ const startMsg = `
     |                                                                     BB-61/
      \\_________________________________________________________________________|
 
-
+                            Game code length: ${codeLength}
+                            Maximum ongoing games: ${maxGames.toLocaleString()}
 `;
+
+const games = new Map();
+const playerIndex = new Map();
+
+class Game {
+    id;
+    lastPing;
+    players;
+
+    constructor(gameID) {
+        this.id = gameID;
+        this.lastPing = Date.now();
+
+        this.players = [...[,,]].map(_ => Game.generatePlayer());
+        this.players.map(player => playerIndex.set(player, gameID));
+
+        log(`new game with id ${gameID}`);
+    }
+
+    ping() {
+        this.lastPing = Date.now();
+    }
+    remove() {
+        this.players.map(player => playerIndex.delete(player));
+    }
+
+    static generateID(length) {
+        return [...Array(length)].map(_ => Math.random() * 10 | 0).join('');
+    }
+    static generatePlayer() {
+        let player = crypto.randomUUID();
+        while (playerIndex.has(player)) {
+            player = crypto.randomUUID();
+        }
+        return player;
+    }
+}
 
 app.get('/', (req, res) => {
     res.send(home);
 });
 
+app.post('/api/new', (req, res, next) => {
+    if (games.size == maxGames) {
+        res.sendStatus(503);
+    } else {
+        next();
+    }
+}, (req, res) => {
+    let gameID = Game.generateID(codeLength);
+    while (games.has(gameID)) {
+        gameID = Game.generateID(codeLength);
+    }
+
+    const game = new Game(gameID);
+    const [hostID] = game.players;
+    games.set(gameID, game);
+
+    res.json({ gameID, hostID });
+});
+
+const api = express.Router();
+app.use('/api/:id', (req, res, next) => {
+    const valid = playerIndex.has(req.params.id);
+    if (valid) {
+        req.user = {
+            game: playerIndex.get(req.params.id),
+            id: games.get(playerIndex.get(req.params.id)).players.indexOf(req.params.id)
+        };
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+});
+
+api.get('/info', (req, res) => {
+    res.send(`User ID is: ${req.user.id}; Game ID: ${req.user.game}`);
+});
+
+app.use('/api/:id', api);
 app.listen(port, console.log(startMsg));
